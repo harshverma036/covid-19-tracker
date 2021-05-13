c036280be97235186571098bb5fc6a59af48d20c
Harsh Verma <harshverma0362@gmail.com>
2021-05-13 21:48:02 +0530

    implimented appbar for md screens

---
+++ src/components/Appbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  Hidden,
+} from "@material-ui/core";
+
+const Appbar = () => {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography
+          variant="h4"
+          component={Link}
+          to="/"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          Covid 19 Tracker
+        </Typography>
+        <Hidden smDown>
+          <Box display="flex" flexDirection="row" ml="auto">
+            <Button
+              variant="contained"
+              color="secondary"
+              //   size="large"
+              style={{ marginRight: 8, fontSize: 16 }}
+            >
+              {"Global"}
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ fontSize: 16 }}
+            >
+              {"Country"}
+            </Button>
+          </Box>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Appbar;
